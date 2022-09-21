f71becc2506fa138e6cafcfa8663a149c61f4540
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-09-21 21:44:18 +0200

    Adding changes from index.html and script.js files

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const canvas = document.getElementById("my_canvas");
+canvas.height = window.innerHeight;
+canvas.width = 200;
 
+const context = canvas.getContext("2d");
+const car = new Car(100, 100, 30, 50);
+
+car.draw(context);
