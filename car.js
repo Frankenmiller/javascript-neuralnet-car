a970306a91fd83ce36e502577740551f5f3b3384
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-09-22 09:25:36 +0200

    Added variables for max speed and for friction

--- car.js
+++ car.js
@@ -4,17 +4,33 @@ class Car {
 		this.y = y;
 		this.width = width;
 		this.height = height;
-
+		this.speed = 0;
+		this.acceleration = 0.2;
+		this.max_speed = 3;
+		this.friction = 0.05;
 		this.controls = new Controls();
 	}
 
 	update() {
-		if(this.controls.forward){
-			this.y-=2;
+		if (this.controls.forward) {
+			this.speed += this.acceleration;
 		}
-		if(this.controls.reverse){
-			this.y+=2;
+		if (this.controls.reverse) {
+			this.speed -= this.acceleration;
 		}
+		if (this.speed > this.max_speed) {
+			this.speed = this.max_speed;
+		}
+		if (this.speed < -this.max_speed / 2) {
+			this.speed = -this.max_speed / 2;
+		}
+		if (this.speed > 0) {
+			this.speed -= this.friction;
+		}
+		if (this.speed < 0) {
+			this.speed += this.friction;
+		}
+		this.y -= this.speed;
 	}
 
 	draw(context) {
