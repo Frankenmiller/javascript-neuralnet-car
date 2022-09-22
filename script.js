5baeddf59ca9d8682b2ad4330d6f293969c2c931
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-09-22 11:37:59 +0200

    Added speedometer display feature

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const canvas = document.getElementById("my_canvas");
+const inst_panel = document.getElementById("my_speedometer");
 canvas.width = 200;
+inst_panel.height = 100;
 
 const context = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const dashboard = inst_panel.getContext("2d");
+const car = new Car(100, 350, 30, 50);
+const speedometer = new Speedometer(10, 10, 50, 50);
 
 animate();
 
 function animate(){
     car.update();
+    speedometer.update();
     canvas.height = window.innerHeight;
+    inst_panel.width = (window.innerWidth / 2) - ((window.innerWidth / 100) * 2) - 100;
     car.draw(context);
+    speedometer.draw(dashboard);
     requestAnimationFrame(animate);
 }
